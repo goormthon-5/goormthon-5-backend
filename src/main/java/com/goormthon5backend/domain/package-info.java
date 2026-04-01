5f9736578c9b423f288753cbc40e29e7de2cfd40
JM_DEV <kyungheeyj45@ajou.ac.kr>
2026-04-01 22:35:52 +0900

    fix: 패키지 구조 세팅

---
+++ src/main/java/com/goormthon5backend/domain/package-info.java
@@ -0,0 +1,1 @@
+package com.goormthon5backend.domain;
--- src/main/java/com/goormthon5backend/global/config/OpenApiConfig.java
+++ src/main/java/com/goormthon5backend/global/config/OpenApiConfig.java
@@ -1,4 +1,4 @@
-package com.goormthon5backend.config;
+package com.goormthon5backend.global.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
