467dbe56704b6da65f4a003179c3c255df4b6a2f
JM_DEV <kyungheeyj45@ajou.ac.kr>
2026-04-03 01:07:52 +0900

    feat: 방명록 작성

--- src/main/java/com/goormthon5backend/web/guest_book/GuestBookController.java
+++ src/main/java/com/goormthon5backend/web/guest_book/GuestBookController.java
@@ -2,16 +2,19 @@ package com.goormthon5backend.web.guest_book;
 
 import com.goormthon5backend.dto.guest_book.GuestBookDto;
 import com.goormthon5backend.service.guest_book.GuestBookService;
+import java.io.IOException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,12 +28,13 @@ public class GuestBookController {
         return guestBookService.getAccommodationGuestBooks(accommodationId);
     }
 
-    @PostMapping
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void create(
         @PathVariable Long accommodationId,
-        @RequestBody GuestBookDto.CreateRequest request
-    ) {
-        guestBookService.createGuestBook(accommodationId, request);
+        @RequestPart("request") GuestBookDto.CreateRequest request,
+        @RequestPart(value = "image", required = false) MultipartFile image
+    ) throws IOException {
+        guestBookService.createGuestBook(accommodationId, request, image);
     }
 }
