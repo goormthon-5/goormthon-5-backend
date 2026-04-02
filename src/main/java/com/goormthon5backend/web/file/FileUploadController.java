39501d4fd3fa62cc0668525cae391f495cf99377
JM_DEV <kyungheeyj45@ajou.ac.kr>
2026-04-02 10:03:23 +0900

    feat: Cors 설정

--- src/main/java/com/goormthon5backend/web/file/FileUploadController.java
+++ src/main/java/com/goormthon5backend/web/file/FileUploadController.java
@@ -2,7 +2,6 @@ package com.goormthon5backend.web.file;
 
 import com.goormthon5backend.service.file.S3FileService;
 import java.io.IOException;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/files")
 public class FileUploadController {
 
