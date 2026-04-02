6eaa8aeeb0f3591d0399f5175c0dca453c5a9075
JM_DEV <kyungheeyj45@ajou.ac.kr>
2026-04-02 21:08:54 +0900

    fix: Mixed Content 에러 해결(Swagger 수정)

--- src/main/java/com/goormthon5backend/global/config/OpenApiConfig.java
+++ src/main/java/com/goormthon5backend/global/config/OpenApiConfig.java
@@ -3,6 +3,8 @@ package com.goormthon5backend.global.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,12 +13,16 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI openAPI() {
-        return new OpenAPI().info(
-                new Info()
-                        .title("Goormthon 5 Backend API")
-                        .version("v1")
-                        .description("Goormthon 5 backend swagger documentation")
-                        .contact(new Contact().name("Backend Team"))
-        );
+        return new OpenAPI()
+                .servers(List.of(
+                        new Server().url("https://goormthon-5.goorm.training")
+                ))
+                .info(
+                        new Info()
+                                .title("Goormthon 5 Backend API")
+                                .version("v1")
+                                .description("Goormthon 5 backend swagger documentation")
+                                .contact(new Contact().name("Backend Team"))
+                );
     }
 }
